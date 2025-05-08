94caaa1d2722904038615f4bbce914c8ba442cae
Vinayak <95471226+psspsss@users.noreply.github.com>
2025-05-08 16:38:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const asciiDiv = document.getElementById("ascii");
 
 // Set video properties
 video.width = 100vh; // Adjust the resolution for ASCII art
-video.height = 80; 
+video.height = 80vh; 
 video.play();
 
 const canvas = document.createElement("canvas");
